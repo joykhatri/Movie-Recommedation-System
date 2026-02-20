6410f0ab58ddad062f917b0b0daf23780b3616bc
joykhatri <joykhatri5@gmail.com>
2026-02-20 18:54:45 +0530

    Add files via upload

--- movie_app/scheduler.py
+++ movie_app/scheduler.py
@@ -7,3 +7,13 @@ def start():
     # scheduler.add_job(fetch_popular_actors, 'interval', minutes=1)
     scheduler.start()
     print("fetching movies")
+
+
+# NOTE: there are 2 fetch trending function in import_movies.py
+# Both are for different purposes one for Movies and second is for TV Shows
+# and i create both with same name, so you cannot run both function at same time 
+# so you have to comment one function and then run the other function
+
+# and you have to run one scheduler at a time
+# if one scheduler is running then comment out the other scheduler
+# because TMDB has limited api calling (40 Requests / min)
